91213a9d4c33a3336e747fa125dc1f1ff52233b1
Tim Holzer <timiholzer@gmail.com>
2020-10-21 19:49:49 -0600

    added a gameplan

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # uofu-proj1
 The first project for U of U's coding bootcamp -TH CH WH HR
+
+
---
+++ script.js
@@ -0,0 +1,6 @@
+// youtube request
+
+//tastdive url example https://tastedive.com/api/similar?q=The+beatles&type=music
+//figure out a way to turn spaces into "+"
+
+// example embed youtube just replace the src ending <iframe width="560" height="315" src="https://www.youtube.com/embed/cSp1dM2Vj48" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
